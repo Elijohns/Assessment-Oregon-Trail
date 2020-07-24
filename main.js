1d2563458829e07bcec51130a5f059e9a912db7b
Beth Johns <eajohns01@gmail.com>
2020-07-23 21:17:37 -0500

    final commit

--- main.js
+++ main.js
@@ -18,6 +18,46 @@ class Traveler {
         
     }   
 }
+    class Doctor extends Traveler {
+        constructor(name) {
+            super(name)
+        
+        }
+        heal(traveler){
+            this.isHealthy = true
+        }
+    }
+    class Hunter extends Traveler {
+        constructor(name) {
+            super(name)
+            this.food = 2
+         }
+
+        hunt() {
+            this.food = this.food += 5
+        }
+    
+    
+
+    eat () {
+        if (this.food >= 2) {
+            this.food -= 2
+        } else {
+            this.isHealthy = false
+            this.food = 0
+        }
+    }
+
+    giveFood(traveler, numOfFoodUnits) {
+        if (this.food >= numOfFoodUnits) {
+            traveler.food += numOfFoodUnits
+            this.food -= numOfFoodUnits
+
+        }
+    }
+}
+    
+
 
 class Wagon {
     constructor (capacity){
